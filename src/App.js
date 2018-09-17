3391c6fec01ce2c1f312f431ea6be18bfbe219ed
HR <hrui1990@gmail.com>
2018-09-17 17:47:51 -0500

    display components conditionally

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React, { Component } from "react";
 import "./App.css";
 import ImageSlider from "./components/ImageSlider";
+import Counter from "./components/Counter";
+import { Header } from "./components/Header";
 
 class App extends Component {
+  state = {
+    visible: true,
+    whichComponentToShow: "ImageSlider"
+  };
   render() {
-    return (
-      <div className="App">
-        <ImageSlider />
-      </div>
-    );
+    // using if statement to hide
+    // used for totally transition
+    // if (!this.state.visible) {
+    //   return <div>Hidden</div>;
+    // }
+    if (this.state.whichComponentToShow === "ImageSlider") {
+      return (
+        <div className="App">
+          <ImageSlider />
+          <button
+            onClick={() => {
+              this.setState({ whichComponentToShow: "Counter" });
+            }}
+          >
+            Show Counter
+          </button>
+        </div>
+      );
+    } else if (this.state.whichComponentToShow === "Counter") {
+      return (
+        <div className="App">
+          <Counter />
+          <button
+            onClick={() => {
+              this.setState({ whichComponentToShow: "Header" });
+            }}
+          >
+            Show Header
+          </button>
+        </div>
+      );
+    } else if (this.state.whichComponentToShow === "Header") {
+      return (
+        <div className="App">
+          <Header />
+        </div>
+      );
+    }
+    return null;
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,6 @@ export class Header extends React.Component {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{this.props.title}</h1>
-        <div>{JSON.stringify(this.props.myObj)}</div>
-        <div>{this.props.myArr[0]}</div>
-        <div>{this.props.myFunc(10, 12)}</div>
-        <div>{this.props.myFunc2(1, 1.2)}</div>
-        <div>{this.props.myFunc3(2, 2.2)}</div>
       </header>
     );
   }
