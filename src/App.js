fbea96f64c98405b5bba4d9e428a4c1e5d3ec037
HR <hrui1990@gmail.com>
2018-09-25 23:37:20 -0500

    add todo app as a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+import TodoList from "./components/TodoList";
 
 class App extends Component {
   state = {
@@ -25,16 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter
-          count={this.state.count}
-          decrementHandler={this.decrementHandler}
-          incrementHandler={this.incrementHandler}
-        />
-        <Counter
-          count={this.state.count - 1}
-          decrementHandler={this.decrementHandler}
-          incrementHandler={this.incrementHandler}
-        />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoList.js
@@ -0,0 +1,34 @@
+/*
+ToDo moveCursor
+1. add todo
+2. display todos
+3. cross off todo
+4. show number of active todos
+5. filter all/active/complete
+6. delete todo
+7. delete all complete (only show if at least one is complete)
+8. button to toggle all on/off
+*/
+
+import React from "react";
+import TodoForm from "./TodoForm";
+
+export default class TodoList extends React.Component {
+  state = {
+    todos: []
+  };
+
+  addTodo = todo => {
+    this.setState({
+      todos: [todo, ...this.state.todos]
+    });
+  };
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {JSON.stringify(this.state.todos)}
+      </div>
+    );
+  }
+}
